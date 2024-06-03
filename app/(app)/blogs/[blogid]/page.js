e43f244aa77730dbb02eb7190f4a4553dedd2a4d
parth299 <sainiparth299@gmail.com>
2024-06-04 01:03:26 +0530

    some issues related to ui fixed

--- app/(app)/blogs/[blogid]/page.js
+++ app/(app)/blogs/[blogid]/page.js
@@ -22,12 +22,12 @@ const Page = () => {
  
   return (
     <>
-      <div className=" min-h-screen w-screen flex justify-center lg:px-20 px-4 absolute top-0 text-black bg-gradient-to-r from-zinc-900 to-black">
+      <div className=" min-h-screen w-screen flex justify-center lg:px-20 px-4 absolute top-0 text-black bg-gradient-to-r from-zinc-900 to-black pb-20">
 
         <div className="mainBlog text-white w-[90%] lg:w-[75%] mt-32 px-3 py-3">
           <h1 className='text-5xl lg:text-6xl text-blue-300'>{title}</h1>
           {/* <hr className='my-3' /> */}
-          <div className='lg:mt-14 mt-8 text-white/60 lg:text-2xl'>{content}</div>
+          <div className='lg:mt-14 mt-8 text-white/60 lg:text-2xl whitespace-pre-wrap'>{content}</div>
         </div>
 
       </div>
